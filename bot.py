81dc134d390a19d67d48472234f4c4b4071be0ab
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-03 09:38:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 #ADMINS = os.getenv("ADMINS")
-ADMINS = 1733124290
+ADMINS = 1821530401
 
 PORT = "8080"
 # MongoDB URI from environment variables
